b3b862648a67474bd36741b4273a95e08a20fb99
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2025-10-23 21:39:26 +0200

    formatting and functionality
    - moved types to a different file
    - updated css
    - added copy option for homepage

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,175 @@
+const DEFAULT_HASH = "#Home";
+const PAGES = {
+    "#Home": {
+        archive: false,
+        rows: []
+    },
+    "#Programmieren": {
+        archive: false,
+        rows: [
+            {
+                name: "GitHub",
+                tiles: [
+                    {
+                        name: "Login",
+                        icon: "icons/github.svg",
+                        link: "https://github.com/login"
+                    },
+                    {
+                        name: "Repos",
+                        icon: "icons/github.svg",
+                        link: "https://github.com/Samuel-Risner?tab=repositories"
+                    },
+                    {
+                        name: "Gist",
+                        icon: "icons/github.svg",
+                        link: "https://gist.github.com/Samuel-Risner"
+                    },
+                ]
+            },
+            {
+                name: "Broswer",
+                tiles: [
+                    {
+                        copy: true,
+                        name: "add extension",
+                        icon: "icons/extension.svg",
+                        link: "about:debugging#/runtime/this-firefox"
+                    },
+                ]
+            },
+            {
+                name: "Help",
+                tiles: [
+                    {
+                        name: "ChatGPT",
+                        icon: "icons/chatgpt.svg",
+                        link: "https://chatgpt.com/"
+                    },
+                ]
+            },
+            {
+                name: "Anderes",
+                tiles: [
+                    {
+                        name: "Python",
+                        icon: "icons/python.svg",
+                        link: "https://www.python.org/"
+                    },
+                    {
+                        name: "tk docs",
+                        icon: "icons/tkdocs.png",
+                        link: "https://tkdocs.com/tutorial/index.html"
+                    },
+                ]
+            },
+        ]
+    },
+    "#FAU": {
+        archive: false,
+        rows: [
+            {
+                name: "StudOn",
+                tiles: [
+                    {
+                        name: "StudOn",
+                        icon: "icons/studon_logo-278x185-294256125.jpg",
+                        link: "https://www.crunchyroll.com/discover"
+                    },
+                ]
+            },
+            {
+                name: "StudOn2",
+                tiles: [
+                    {
+                        name: "StudOn2",
+                        icon: "icons/studon_logo-278x185-294256125.jpg",
+                        link: "https://www.crunchyroll.com/discover"
+                    },
+                ]
+            },
+        ]
+    },
+    "#ICH": {
+        archive: false,
+        rows: [
+            {
+                name: "Anime & Manga",
+                tiles: [
+                    {
+                        name: "Chrunchyroll",
+                        icon: "icons/crunchyroll.svg",
+                        link: "https://www.crunchyroll.com"
+                    },
+                    {
+                        name: "Tappytoon",
+                        icon: "icons/tappytoon.png",
+                        link: "https://www.tappytoon.com/en/comics/home"
+                    },
+                    {
+                        name: "Solo Leveling",
+                        icon: "icons/tappytoon.png",
+                        link: "https://www.tappytoon.com/en/book/solo-leveling-official"
+                    },
+                ]
+            },
+        ]
+    },
+    "#Kirche": {
+        archive: false,
+        rows: [
+            {
+                name: "De",
+                tiles: [
+                    {
+                        name: "Home",
+                        icon: "icons/kirche2.png",
+                        link: "https://www.churchofjesuschrist.org/?lang=deu"
+                    },
+                    {
+                        name: "Archiv",
+                        icon: "icons/kirche2.png",
+                        link: "https://www.churchofjesuschrist.org/study?lang=deu&platform=web"
+                    },
+                ]
+            },
+            {
+                name: "En",
+                tiles: [
+                    {
+                        name: "Home",
+                        icon: "icons/kirche2.png",
+                        link: "https://www.churchofjesuschrist.org/?lang=eng"
+                    },
+                    {
+                        name: "Library",
+                        icon: "icons/kirche2.png",
+                        link: "https://www.churchofjesuschrist.org/study?lang=eng&platform=web"
+                    },
+                ]
+            },
+            {
+                name: "Pt",
+                tiles: [
+                    {
+                        name: "Home",
+                        icon: "icons/kirche2.png",
+                        link: "https://www.churchofjesuschrist.org/?lang=por"
+                    },
+                    {
+                        name: "Biblioteca",
+                        icon: "icons/kirche2.png",
+                        link: "https://www.churchofjesuschrist.org/study?lang=por&platform=web"
+                    },
+                ]
+            },
+        ]
+    },
+};
 const ROOT_ELEMENT = document.getElementById("root");
-const DEFAULT_HASH = "#HOME";
 const PAGE_HTMLElements = {};
 let lastHash = DEFAULT_HASH;
 let currentHash = DEFAULT_HASH;
-const PAGES = {};
 function tw(tailwindCSS) { return tailwindCSS; }
 function loadCurrentHash() {
     currentHash = window.location.hash;
@@ -69,18 +235,26 @@ function initializePages() {
             rowNameDisplay.textContent = row.name;
             rowNameDisplay.className = tw("size-24 bg-gray-400 flex items-center justify-center");
             row.tiles.forEach(tile => {
-                const link = document.createElement("a");
-                rowContainer.appendChild(link);
-                link.href = tile.link;
-                link.target = "_blank";
+                let link = document.createElement("a");
+                if (tile.copy) {
+                    link = document.createElement("button");
+                    link.onclick = () => {
+                        navigator.clipboard.writeText(tile.link);
+                    };
+                }
+                else {
+                    link.href = tile.link;
+                    link.target = "_blank";
+                }
                 link.className = tw("w-24 h-32");
+                rowContainer.appendChild(link);
                 const iconContainer = document.createElement("div");
                 link.appendChild(iconContainer);
                 iconContainer.className = tw("size-24 flex items-center justify-center p-2 bg-gray-200");
                 const icon = document.createElement("img");
                 iconContainer.appendChild(icon);
                 icon.src = tile.icon;
-                icon.className = tw("object-contain");
+                icon.className = tw("object-contain max-w-20 max-h-20");
                 const description = document.createElement("div");
                 link.appendChild(description);
                 description.textContent = tile.name;
---
+++ src/types.d.ts
@@ -0,0 +1,20 @@
+export type T_TILE = {
+    copy?: true,
+    name: string,
+    icon: string,
+    link: string,
+}
+
+export type T_ROW = {
+    name: string,
+    tiles: T_TILE[]
+}
+
+export type T_PAGE = {
+    archive?: boolean,
+    rows: T_ROW[]
+}
+
+export type T_PAGES = {
+    [key: string]: T_PAGE
+}
